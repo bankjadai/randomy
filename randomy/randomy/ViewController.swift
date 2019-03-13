5a4bf9418c8566ebd197873e2b1c9207b2ab508c
student <student@informaticss-Mac-mini.local>
2019-03-13 10:56:26 +0700

    first commit

---
+++ randomy/randomy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomy
+//
+//  Created by student on 13/3/2562 BE.
+//  Copyright © 2562 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
